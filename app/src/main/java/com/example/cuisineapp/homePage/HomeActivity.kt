ed870c84a917ccfa6553e0c472e4da57e5554b2f
Max <maxime.alfosea@hotmail.fr>
2019-06-25 13:32:11 +0200

    Clean Architecture base

--- app/src/main/java/com/example/cuisineapp/homePage/HomeActivity.kt
+++ app/src/main/java/com/example/cuisineapp/homePage/HomeActivity.kt
@@ -1,9 +1,10 @@
-package com.example.cuisineapp
+package com.example.cuisineapp.homePage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cuisineapp.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/cuisineapp/homePage/HomePresenter.kt
@@ -0,0 +1,4 @@
+package com.example.cuisineapp.homePage
+
+class HomePresenter {
+}
