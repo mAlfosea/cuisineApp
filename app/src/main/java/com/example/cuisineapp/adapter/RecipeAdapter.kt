e98190377b2e5bbd8df8cbbc3c55db5e7e352449
Rwog <aviles.jerome@gmail.com>
2019-06-27 14:46:05 +0200

    Mise en place des images

--- app/src/main/java/com/example/cuisineapp/adapter/RecipeAdapter.kt
+++ app/src/main/java/com/example/cuisineapp/adapter/RecipeAdapter.kt
@@ -1,14 +1,17 @@
 package com.example.cuisineapp.adapter
 
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cuisineapp.R
+import com.example.cuisineapp.model.Recipe
 import kotlinx.android.synthetic.main.cellview.view.*
-import kotlinx.android.synthetic.main.item_recipe.view.*
 
-class RecipeAdapter(var recipes: List<Int>): RecyclerView.Adapter<RecipeAdapter.ViewHolder>() {
+
+
+class RecipeAdapter(var recipes: Array<Recipe>): RecyclerView.Adapter<RecipeAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val lineView = LayoutInflater.from(parent.context).inflate(R.layout.cellview, parent, false)
         return ViewHolder(lineView)
@@ -23,8 +26,26 @@ class RecipeAdapter(var recipes: List<Int>): RecyclerView.Adapter<RecipeAdapter.
     }
 
     class ViewHolder (var view: View): RecyclerView.ViewHolder(view) {
-        fun bindRecipe(recipe: Int) {
-            itemView.recipeText.text = recipe.toString()
+        fun bindRecipe(recipe: Recipe) {
+
+            var assetMan = view.context.assets
+
+            with (recipe) {
+
+                var idRes:Int = 0
+                when (recipe_picture) {
+                    "a.jpg" -> idRes = R.drawable.a
+                    "b.jpg" -> idRes = R.drawable.b
+                    "c.jpg" -> idRes = R.drawable.c
+                    "d.jpg" -> idRes = R.drawable.d
+                    "e.jpg" -> idRes = R.drawable.e
+                    "f.jpg" -> idRes = R.drawable.f
+                    else -> idRes = 0
+                }
+
+                itemView.recipeText.text = recipe_name
+                itemView.recipeImage.setImageResource(idRes)
+            }
         }
     }
 }
--- app/src/main/java/com/example/cuisineapp/homePage/HomeActivity.kt
+++ app/src/main/java/com/example/cuisineapp/homePage/HomeActivity.kt
@@ -1,10 +1,12 @@
 package com.example.cuisineapp.homePage
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.cuisineapp.R
 import com.example.cuisineapp.adapter.RecipeAdapter
+import com.example.cuisineapp.model.DataManager
 import kotlinx.android.synthetic.main.activity_show_all_recipes.*
 
 class HomeActivity : AppCompatActivity() {
@@ -13,9 +15,10 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_all_recipes)
 
+        setContentView(R.layout.activity_show_all_recipes)
+        DataManager.initWithJSONFile("begin_json.json", this)
         ui_viewResult.layoutManager = GridLayoutManager(this, 3)
-        ui_viewResult.adapter = RecipeAdapter(recipeList)
+        ui_viewResult.adapter = RecipeAdapter(DataManager.dataRecipe)
     }
 }
